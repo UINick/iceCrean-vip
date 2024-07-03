7611695f9885e2c891fe3508b487236975f77e86
UINick <nicholas_developer@outlook.com>
2024-07-03 10:40:19 -0300

    starting vip example

---
+++ IceCream/IceCream/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IceCream
+//
+//  Created by Nicholas Forte on 03/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
